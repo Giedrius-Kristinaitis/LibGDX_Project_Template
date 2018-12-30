eb3ae7819552c0810ae4ca7fbdfb57838555ce35
Giedrius Kristinaitis <36892255+Giedrius-Kristinaitis@users.noreply.github.com>
2018-12-30 23:03:05 +0200

    Add files via upload

---
+++ src/com/template/game/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.template.game.utils;
+
+/**
+ * Contains global game constants
+ */
+public class Constants {
+
+    public static final float WIDTH = 25; // width of the game world
+    public static final float HEIGHT = 45; // height of the game world
+}
